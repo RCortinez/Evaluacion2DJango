499a0908ea7bdfa617c351780163446084d8ca3b
RCortinez <58152747+RCortinez@users.noreply.github.com>
2019-11-27 02:17:27 -0300

    Add files via upload

--- Proyectos/mysite/blog/urls.py
+++ Proyectos/mysite/blog/urls.py
@@ -16,9 +16,8 @@ Including another URLconf
 
 #from django.urls import path
 from django.conf.urls import include, url
-from django.contrib import admin
+from . import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'', include('blog.urls')),
+    url(r'^$', views.post_list),
 ]
